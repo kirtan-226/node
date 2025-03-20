65d94526582fc6f6aa85c088c4bf288c89f3a1fc
kirtan Limbachiya <108492403+kirtan-226@users.noreply.github.com>
2025-03-21 00:49:28 +0100

    adding ec2 path in react

--- frontend/src/components/UserQuestionnaire.js
+++ frontend/src/components/UserQuestionnaire.js
@@ -35,7 +35,7 @@ const SurveyQuestions = () => {
         }
 
         const response = await axios.post(
-            'http://localhost:5000/api/survey/post_answers',
+            'http://15.207.254.20:5000/api/survey/post_answers',
             { answers, user_id },
         );
 
